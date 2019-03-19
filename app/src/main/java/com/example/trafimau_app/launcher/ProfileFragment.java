94390bb69782ae013ddbca581e968ee78ee61be3
Aliaksandr Trafimau <cncntrt@gmail.com>
2019-03-19 16:19:34 +0300

    task5: completed profile page

--- app/src/main/java/com/example/trafimau_app/launcher/ProfileFragment.java
+++ app/src/main/java/com/example/trafimau_app/launcher/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.trafimau_app.launcher;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -15,6 +17,8 @@ import android.view.ViewGroup;
 import com.example.trafimau_app.MyApplication;
 import com.example.trafimau_app.R;
 import com.yandex.metrica.YandexMetrica;
+
+import java.util.Locale;
 
 public class ProfileFragment extends Fragment {
 
@@ -60,7 +64,46 @@ public class ProfileFragment extends Fragment {
             fragmentActionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        setOnClickListeners(rootView);
+
         return rootView;
+    }
+
+    private void setOnClickListeners(View rootView) {
+        rootView.findViewById(R.id.mobilePhone).setOnClickListener(v -> {
+            Uri uri = Uri.parse("tel:" + getString(R.string.mobilePhoneNumber));
+            Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+            startActivity(intent);
+        });
+
+        rootView.findViewById(R.id.workPhone).setOnClickListener(v -> {
+            Uri uri = Uri.parse("tel:" + getString(R.string.workPhoneNumber));
+            Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+            startActivity(intent);
+        });
+
+        rootView.findViewById(R.id.email).setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:" + getString(R.string.email)));
+            startActivity(intent);
+        });
+
+        rootView.findViewById(R.id.github).setOnClickListener(v -> {
+            Uri uri = Uri.parse("https://www.github.com/" + getString(R.string.githubProfile));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        });
+
+
+        rootView.findViewById(R.id.inspiringPlace).setOnClickListener(v -> {
+            final double latitude = 38.48247594504772;
+            final double longitude = 22.500576004385948;
+            String stringUri = String.format(
+                    Locale.getDefault(),
+                    "geo:%f,%f?q=Ancient+Theatre,Delphi,Greece", latitude, longitude);
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(stringUri));
+            startActivity(intent);
+        });
     }
 
     @Override
